9adaca6055387dd2ad15cfaf431b197b2b055680
Lyubomir Kiprov <lyubo@limechain.tech>
2019-05-14 12:34:22 +0300

    Remove unused helper code

--- src/helpers/crypto.js
+++ src/helpers/crypto.js
@@ -1,45 +1,5 @@
 const cryptoJS = require('crypto-js');
 
-// module.exports = {
-//     encrypt: function (account, password) {
-//         try {
-//             let dataToBeEncrypted = JSON.stringify({
-//                 data
-//             });
-
-//             let dataHash = hashAccount(dataToBeEncrypted);
-//             let ciphertext = encryptAccountData(`${data.privateKey}::${dataHash}`, password);
-
-//             return JSON.stringify({
-//                 ...data,
-//                 ciphertext
-//             });
-//         } catch (error) {
-//             throw new Error(`Account encryption: ${error.message}`);
-//         }
-//     },
-//     decrypt: function (encryptedAccount, password) {
-//         try {
-//             let decryptedAccount = JSON.parse(JSON.stringify(encryptedAccount));
-//             delete decryptedAccount.ciphertext;
-
-//             let pureData = decryptedAccountData(encryptedAccount.ciphertext, password);
-//             let dataParts = pureData.split('::');
-
-//             decryptedAccount.privateKey = dataParts[0];
-//             let dataHash = hashAccount(decryptedAccount);
-
-//             if (dataHash != dataParts[1]) {
-//                 throw new Error('Broken account. Most of time reason: invalid network');
-//             }
-
-//             return decryptedAccount.privateKey;
-//         } catch (error) {
-//             throw new Error(`Account decryption: ${error.message}`);
-//         }
-//     }
-// }
-
 module.exports = {
     hash: function (data) {
         try {
