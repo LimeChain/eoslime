fac893a5625fa19ea43a3623c96d4cf4b0a36a2b
bakasura980 <lyubo@limechain.tech>
2019-09-13 10:34:44 +0300

    Add tests

--- tests/testing-contracts/faucet.cpp
+++ tests/testing-contracts/faucet.cpp
@@ -3,47 +3,59 @@
 
 using namespace eosio;
 
-CONTRACT faucet : public contract {
+CONTRACT faucet : public contract
+{
 
-  public:
-  
-    using contract::contract;
+public:
+	using contract::contract;
 
-    faucet(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
-                                                                     withdrawersTable(receiver, code.value)
-    {}
+	faucet(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds),
+																	withdrawersTable(receiver, code.value)
+	{
+	}
 
-    TABLE withdrawer
-    {
-        uint64_t account;
-        asset quantity;
-        std::string token_name;
-        std::string memo;
+	TABLE withdrawer
+	{
+		uint64_t account;
+		asset quantity;
+		std::string token_name;
+		std::string memo;
 
-        uint64_t primary_key() const { return account; }
-    };
-    typedef eosio::multi_index<"withdrawers"_n, withdrawer> withdrawers;
-    withdrawers withdrawersTable;
+		uint64_t primary_key() const { return account; }
+	};
+	typedef eosio::multi_index<"withdrawers"_n, withdrawer> withdrawers;
+	withdrawers withdrawersTable;
 
+	ACTION produce(name to, asset quantity, std::string token_name, std::string memo)
+	{
+		auto withdrawal_itr = withdrawersTable.find(to.value);
+		if (withdrawal_itr != withdrawersTable.end())
+		{
+			withdrawersTable.modify(withdrawal_itr, _self, [&](auto &withdrawal) {
+				withdrawal.quantity.amount += quantity.amount;
+			});
+		}
+		else
+		{
+			withdrawersTable.emplace(_self, [&](auto &new_withdrawal) {
+				new_withdrawal.account = to.value;
+				new_withdrawal.quantity = quantity;
+				new_withdrawal.token_name = token_name;
+				new_withdrawal.memo = memo;
+			});
+		}
+	}
 
-    ACTION produce(name to, asset quantity, std::string token_name, std::string memo) {
-
-      withdrawersTable.emplace(_self, [&](auto &s) {
-        s.account = to.value;
-        s.quantity = quantity;
-        s.token_name = token_name;
-        s.memo = memo;
-      });
-    }
-
-    
-    ACTION withdraw(name withdrawer) {
-      // Inline action (Call token contract)
-      for(auto& currentWithdrawer : withdrawersTable) {
-        if (currentWithdrawer.account == withdrawer.value) {
-            token::issue_action issue(currentWithdrawer.token_name, {get_self(), "active"_n});
-            issue.send(withdrawer, currentWithdrawer.quantity, currentWithdrawer.memo);
-        }
-      }
-    }
+	ACTION withdraw(name withdrawer)
+	{
+		// Inline action (Call token contract)
+		for (auto &currentWithdrawer : withdrawersTable)
+		{
+			if (currentWithdrawer.account == withdrawer.value)
+			{
+				token::issue_action issue(currentWithdrawer.token_name, {get_self(), "active"_n});
+				issue.send(withdrawer, currentWithdrawer.quantity, currentWithdrawer.memo);
+			}
+		}
+	}
 };
