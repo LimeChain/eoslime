52d7dc1ad5beb8473c0530018546f5caa6ca1b4a
bakasura980 <lyubo@limechain.tech>
2019-11-29 12:17:15 +0200

    Support tokens payments for contarct actions

--- src/contract/contract-function/function-optionals/index.js
+++ src/contract/contract-function/function-optionals/index.js
@@ -1,8 +1,10 @@
 const fromOptional = require("./from");
 const uniqueOptional = require("./unique");
+const tokensOptional = require('./tokens');
 
 module.exports = {
     from: fromOptional,
     unique: uniqueOptional,
-    all: [fromOptional, uniqueOptional]
+    tokens: tokensOptional,
+    all: [fromOptional, uniqueOptional, tokensOptional]
 };
---
+++ src/contract/contract-function/function-optionals/tokens.js
@@ -0,0 +1,18 @@
+const tokensOptional = function (optionals, rawTransaction) {
+    if (optionals && optionals.tokens) {
+
+        rawTransaction.actions.push({
+            account: "eosio.token",
+            name: "transfer",
+            authorization: [rawTransaction.defaultExecutor.executiveAuthority],
+            data: {
+                from: rawTransaction.defaultExecutor.name,
+                to: rawTransaction.actions[0].account,
+                quantity: optionals.tokens,
+                memo: ""
+            }
+        });
+    }
+};
+
+module.exports = tokensOptional;
