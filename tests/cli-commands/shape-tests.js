4048a5db30fb6c79be81011d239a3394aa546cbf
vladimirhristov <vlado.og.hristov@gmail.com>
2020-05-29 20:56:06 +0300

    cli command init - tests

--- tests/cli-commands/shape-tests.js
+++ tests/cli-commands/shape-tests.js
@@ -6,6 +6,7 @@ const Command = require('../../cli-commands/commands/command');
 const ShapeCommand = require('../../cli-commands/commands/shape/index');
 const GroupCommand = require('../../cli-commands/commands/group-command');
 const definition = require('../../cli-commands/commands/shape/definition');
+const repositories = require('../../cli-commands/commands/shape/specific/repositories.json');
 const FrameworkOption = require('../../cli-commands/commands/shape/options/framework-option');
 
 describe('ShapeCommand', function () {
@@ -28,30 +29,19 @@ describe('ShapeCommand', function () {
     });
 
     function prepareArgs (framework = FRAMEWORK_REACT) {
-        return { _: [ 'shape' ], framework: framework, '$0': 'eoslime' };
+        return { framework: framework };
     }
 
-    it('Should initialize command propertly', async () => {
+    it('Should initialize command properly', async () => {
         assert(shapeCommand instanceof Command);
         assert(shapeCommand.template == definition.template);
         assert(shapeCommand.description = definition.description);
         assert(shapeCommand.options == definition.options);
     });
 
-    it('Should not configure any subcommands', async () => {
+    it('Should not have any subcommands', async () => {
         assert(!(shapeCommand instanceof GroupCommand));
         assert(shapeCommand.subcommands.length == 0);
-    });
-
-    it('Should shape a dApp with default React front-end', async () => {
-        sinon.stub(Git.prototype, 'clone').callsFake(function (command, callback) {
-            callback.call();
-        });
-        
-        assert(await shapeCommand.execute(prepareArgs()));
-
-        sinon.assert.calledWith(processOptionsSpy, prepareArgs());
-        sinon.assert.calledWith(processSpy, FRAMEWORK_REACT);
     });
 
     it('Should shape a dApp with React front-end', async () => {
@@ -63,6 +53,7 @@ describe('ShapeCommand', function () {
 
         sinon.assert.calledWith(processOptionsSpy, prepareArgs());
         sinon.assert.calledWith(processSpy, FRAMEWORK_REACT);
+        assert(processSpy.returnValues[0] == repositories[FRAMEWORK_REACT]);
     });
 
     it('Should throw when an unknown front-end framework is specified', async () => {
@@ -72,11 +63,12 @@ describe('ShapeCommand', function () {
 
         sinon.assert.calledWith(processOptionsSpy, prepareArgs(FRAMEWORK_ANGULAR));
         sinon.assert.calledWith(processSpy, FRAMEWORK_ANGULAR);
+        assert(processSpy.returnValues[0] == repositories[FRAMEWORK_ANGULAR]);
         sinon.assert.notCalled(cloneSpy);
     });
 
     it('Should throw when repository cloning fails', async () => {
-        sinon.stub(Git.prototype, 'clone').throws('Test Failure');
+        sinon.stub(Git.prototype, 'clone').throws('Test: Cloning Repo Failure');
 
         assert(await shapeCommand.execute(prepareArgs()));
     });
