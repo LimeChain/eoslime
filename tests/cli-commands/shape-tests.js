0a5915f175445ca8f60a3ebb558cb873d7e2bb2f
vladimirhristov <vlado.og.hristov@gmail.com>
2020-06-08 18:50:44 +0300

    updated tests for cli command shape

--- tests/cli-commands/shape-tests.js
+++ tests/cli-commands/shape-tests.js
@@ -4,73 +4,60 @@ const assert = require('assert');
 const Git = require('simple-git/src/git');
 const Command = require('../../cli-commands/commands/command');
 const ShapeCommand = require('../../cli-commands/commands/shape/index');
-const GroupCommand = require('../../cli-commands/commands/group-command');
 const definition = require('../../cli-commands/commands/shape/definition');
 const repositories = require('../../cli-commands/commands/shape/specific/repositories.json');
 const FrameworkOption = require('../../cli-commands/commands/shape/options/framework-option');
 
 describe('ShapeCommand', function () {
     const FRAMEWORK_REACT = "react";
-    const FRAMEWORK_ANGULAR = "angular";
+    const NOT_SUPPORTED_FRAMEWORK = "angular";
 
     let shapeCommand;
-    let cloneSpy;
-    let processSpy;
-    let processOptionsSpy;
+    let simpleGitSpy;
+    let frameworkOptionSpy;
 
     beforeEach(async () => {
         shapeCommand = new ShapeCommand();
-        processSpy = sinon.spy(FrameworkOption, "process");
-        processOptionsSpy = sinon.spy(Command.prototype, "processOptions");
+        frameworkOptionSpy = sinon.spy(FrameworkOption, "process");
     });
 
     afterEach(async () => {
         sinon.restore();
     });
 
-    function prepareArgs (framework = FRAMEWORK_REACT) {
-        return { framework: framework };
-    }
-
     it('Should initialize command properly', async () => {
         assert(shapeCommand instanceof Command);
         assert(shapeCommand.template == definition.template);
         assert(shapeCommand.description = definition.description);
         assert(shapeCommand.options == definition.options);
-    });
-
-    it('Should not have any subcommands', async () => {
-        assert(!(shapeCommand instanceof GroupCommand));
         assert(shapeCommand.subcommands.length == 0);
     });
 
-    it('Should shape a dApp with React front-end', async () => {
+    it('Should prepare React project', async () => {
         sinon.stub(Git.prototype, 'clone').callsFake(function (command, callback) {
             callback.call();
         });
         
-        assert(await shapeCommand.execute(prepareArgs()));
+        assert(await shapeCommand.execute({ framework: FRAMEWORK_REACT }));
 
-        sinon.assert.calledWith(processOptionsSpy, prepareArgs());
-        sinon.assert.calledWith(processSpy, FRAMEWORK_REACT);
-        assert(processSpy.returnValues[0] == repositories[FRAMEWORK_REACT]);
+        sinon.assert.calledWith(frameworkOptionSpy, FRAMEWORK_REACT);
+        assert(frameworkOptionSpy.returnValues[0] == repositories[FRAMEWORK_REACT]);
     });
 
     it('Should throw when an unknown front-end framework is specified', async () => {
-        cloneSpy = sinon.spy(Git.prototype, 'clone');
+        simpleGitSpy = sinon.spy(Git.prototype, 'clone');
         
-        assert(await shapeCommand.execute(prepareArgs(FRAMEWORK_ANGULAR)));
+        assert(await shapeCommand.execute({ framework: NOT_SUPPORTED_FRAMEWORK }));
 
-        sinon.assert.calledWith(processOptionsSpy, prepareArgs(FRAMEWORK_ANGULAR));
-        sinon.assert.calledWith(processSpy, FRAMEWORK_ANGULAR);
-        assert(processSpy.returnValues[0] == repositories[FRAMEWORK_ANGULAR]);
-        sinon.assert.notCalled(cloneSpy);
+        sinon.assert.calledWith(frameworkOptionSpy, NOT_SUPPORTED_FRAMEWORK);
+        assert(frameworkOptionSpy.returnValues[0] == repositories[NOT_SUPPORTED_FRAMEWORK]);
+        sinon.assert.notCalled(simpleGitSpy);
     });
 
     it('Should throw when repository cloning fails', async () => {
         sinon.stub(Git.prototype, 'clone').throws('Test: Cloning Repo Failure');
 
-        assert(await shapeCommand.execute(prepareArgs()));
+        assert(await shapeCommand.execute({ framework: FRAMEWORK_REACT }));
     });
 
 });
