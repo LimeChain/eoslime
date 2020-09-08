9c395b36c743f061f556374fd05a96eef8b5e1d5
prc <prcolaco@gmail.com>
2020-09-08 08:40:35 +0100

    refactor expectAssert

--- cli-commands/commands/test/specific/utils/index.js
+++ cli-commands/commands/test/specific/utils/index.js
@@ -1,11 +1,17 @@
 const assert = require('assert');
 
 module.exports = {
-    expectAssert: async function (promise, message) {
-        return expectEOSError(promise, 'eosio_assert_message_exception', 'assert', message);
+    expectAssert: async function (promise, errorMessage) {
+        const result = await expectEOSError(promise, 'eosio_assert_message_exception', 'assert');
+        if (errorMessage && result.error) {
+            result.error = result.message.search(errorMessage) >= 0;
+            result.text = `Expected assert with '${errorMessage}' message, got '${result.message}' instead`;
+        }
+        assert(result.error, result.text);
     },
     expectMissingAuthority: async function (promise) {
-        return expectEOSError(promise, 'missing_auth_exception', 'missing authority');
+        const result = await expectEOSError(promise, 'missing_auth_exception', 'missing authority');
+        assert(result.error, result.text);
     },
     // Todo: Uncomment it once test cli command is ready
     // createTestingAccounts: async function () {
@@ -20,18 +26,17 @@ module.exports = {
     // }
 }
 
-const expectEOSError = async function (promise, errorType, errorInfo, errorMessage) {
+const expectEOSError = async function (promise, errorType, errorInfo) {
     try {
         await promise;
     } catch (error) {
         const message = error.message || error;
         const expectedError = message.search(errorType) >= 0;
-        assert(expectedError, `Expected ${errorInfo}, got '${message}' instead`);
-        if (errorMessage) {
-            const expectedMessage = message.search(errorMessage) >= 0;
-            assert(expectedMessage, `Expected ${errorInfo} with '${errorMessage}' message, got '${message}' instead`);
-        }
-        return;
+        return {
+            message,
+            error: expectedError,
+            text: `Expected ${errorInfo}, got '${message}' instead`
+        };
     }
     assert.fail(`Expected ${errorInfo} not received`);
 }
